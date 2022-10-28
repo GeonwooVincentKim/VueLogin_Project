8f304e2c402e6c28a154d6168ba5c4c3ba127bc9
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-28 16:59:47 +0900

    [Feature] - App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,22 @@
         fixed
         app
     >
-    <v-list>
+    <v-list dense>
         <v-list-item :to="{path: '/home'}">
-        <v-list-item-action>
-            <v-icon>home</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-        </v-list-item-content>
+            <v-list-item-action>
+                <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
         </v-list-item>
         <v-list-item :to="{path: '/login'}">
-        <v-list-item-action>
-            <v-icon>login</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-            <v-list-item-title>Login</v-list-item-title>
-        </v-list-item-content>
+            <v-list-item-action>
+                <v-icon>login</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title>Login</v-list-item-title>
+            </v-list-item-content>
         </v-list-item>
     </v-list>
     </v-navigation-drawer>
@@ -30,31 +30,6 @@
     </v-toolbar>
     <v-content>
         <router-view />
-        <v-container fluid fill-height>
-            <v-layout
-            justify-center
-            align-center
-            >
-            <v-flex text-xs-center>
-                <v-tooltip left>
-                <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                    <v-icon large>code</v-icon>
-                    </v-btn>
-                </template>
-                <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                <template v-slot:activator="{ on }">
-                    <v-btn icon large href="https://codepen.io/johnjleider/pen/rJdVMq" target="_blank" v-on="on">
-                    <v-icon large>mdi-codepen</v-icon>
-                    </v-btn>
-                </template>
-                <span>Codepen</span>
-                </v-tooltip>
-            </v-flex>
-            </v-layout>
-        </v-container>
     </v-content>
     <v-footer color="indigo" app>
     <span class="white--text">&copy; 2017</span>
