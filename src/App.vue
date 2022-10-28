c8f8e26e06ba63d35ea868c5da8eab8daff36974
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-28 13:28:17 +0900

    [Feature] - Uploaded file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,83 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+<v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        app
+    >
+    <v-list>
+        <v-list-item :to="{path: '/home'}">
+        <v-list-item-action>
+            <v-icon>home</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+        </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{path: '/login'}">
+        <v-list-item-action>
+            <v-icon>login</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+            <v-list-item-title>Login</v-list-item-title>
+        </v-list-item-content>
+        </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+        <router-view />
+        <v-container fluid fill-height>
+            <v-layout
+            justify-center
+            align-center
+            >
+            <v-flex text-xs-center>
+                <v-tooltip left>
+                <template v-slot:activator="{ on }">
+                    <v-btn :href="source" icon large target="_blank" v-on="on">
+                    <v-icon large>code</v-icon>
+                    </v-btn>
+                </template>
+                <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                <template v-slot:activator="{ on }">
+                    <v-btn icon large href="https://codepen.io/johnjleider/pen/rJdVMq" target="_blank" v-on="on">
+                    <v-icon large>mdi-codepen</v-icon>
+                    </v-btn>
+                </template>
+                <span>Codepen</span>
+                </v-tooltip>
+            </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+    <span class="white--text">&copy; 2017</span>
+    </v-footer>
+</v-app>
 </template>
 
 <script>
-export default {
-  name: "App",
+import LoginView from './views/LoginView.vue';
 
-  data: () => ({
-    //
-  }),
-};
+export default {
+    name: 'App',
+
+    data: () => ({
+        drawer: null,
+        items: [
+            {
+                
+            }
+        ]
+    }),
+    props: {
+        source: String
+    }
+}
 </script>
